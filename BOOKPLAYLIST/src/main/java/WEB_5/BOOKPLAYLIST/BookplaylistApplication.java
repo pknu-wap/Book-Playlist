6011dbf74af239d104ecd82e4507819c1f29c077
구진모 <jinmo5845@naver.com>
2024-10-01 16:34:57 +0900

    개발 환경 구축

---
+++ BOOKPLAYLIST/src/main/java/WEB_5/BOOKPLAYLIST/BookplaylistApplication.java
@@ -0,0 +1,13 @@
+package WEB_5.BOOKPLAYLIST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookplaylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookplaylistApplication.class, args);
+	}
+
+}
---
+++ BOOKPLAYLIST/src/test/java/WEB_5/BOOKPLAYLIST/BookplaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package WEB_5.BOOKPLAYLIST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookplaylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
