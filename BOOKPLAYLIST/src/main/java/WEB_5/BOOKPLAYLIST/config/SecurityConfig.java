d7635adae0c4cdf41dba7629acb686b46d2f6a1f
구진모 <jinmo5845@naver.com>
2024-11-01 12:15:27 +0900

    .

--- BOOKPLAYLIST/src/main/java/WEB_5/BOOKPLAYLIST/config/SecurityConfig.java
+++ BOOKPLAYLIST/src/main/java/WEB_5/BOOKPLAYLIST/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -13,8 +12,8 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -27,9 +26,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/playlist/**").authenticated() // 세션 로그인 필요
                         .anyRequest().permitAll()
                 )
-                .formLogin(); // 폼 기반 인증 활성화
-
-        http.httpBasic(AbstractHttpConfigurer::disable); // HTTP Basic 인증 비활성화
+                .httpBasic();
 
         return http.build();
     }
@@ -37,7 +34,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://book-playlist.netlify.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://book-playlist.netlify.app/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
