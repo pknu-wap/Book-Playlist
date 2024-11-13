01256162cfb73c73cb7ce7cfe004cec622b204d8
pknujaewan <163826142+pknujaewan@users.noreply.github.com>
2024-11-13 20:07:40 +0900

    fix:playlist 각 텍스트 넘침 수정, Mypage 요소 위치 수정

--- src/Mypage/Mypage.js
+++ src/Mypage/Mypage.js
@@ -4,16 +4,20 @@ import './Mypage.css'
 const MyPage = () => {
   return (
     <div>
-      <div className='userimage'>
+      <div className='topbox'>
+        <div className='userbox'>
+         <div className='userimage'>
+         </div>
+         <div className='username'>
+         <p>username</p>
+         </div>
+        </div>
+       <div className='Mycomment'>
+        <p>내가 쓴 댓글</p>
+       </div>
       </div>
       <div className='Mycollection'>
         <p>내서재</p>
-      </div>
-      <div className='Mycomment'>
-        <p>내가 쓴 댓글</p>
-      </div>
-      <div className='username'>
-        <p>username</p>
       </div>
     </div>
   );
